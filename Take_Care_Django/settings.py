391b96d78d264b5024b9bb0e61507befa5dc493e
EC2 Default User <ec2-user@ip-172-31-42-25.eu-west-1.compute.internal>
2024-04-02 12:20:12 +0000

    updating files

--- Take_Care_Django/settings.py
+++ Take_Care_Django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g8h9r08bp_5dihttd=k!$q!ggm%3x1ebvc14s!d)+^+(5#11yk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'www.welcome.take.care@gmail.com'
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
