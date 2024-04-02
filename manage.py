a6ba0221586c129467aaf0266d717df158f5cbd1
EC2 Default User <ec2-user@ip-172-31-42-25.eu-west-1.compute.internal>
2024-04-02 22:55:52 +0000

    pylint

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
 """ A file automatically created in each Django project. """
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
-def main():
-    """Run administrative tasks."""
+if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Take_Care_Django.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -17,7 +14,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
